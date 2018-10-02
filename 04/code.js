e2894f4abdd46e47748cfb4532895171e2a9bbdf
Nick Sawe <skelmdev@gmail.com>
2018-10-02 16:14:06 +0100

    Day 4 complete

--- 04/code.js
+++ 04/code.js
@@ -1,13 +1,18 @@
-function calculate_md5(params) {
+function calculate_md5(params, number_of_leading_zeroes) {
+    let required_zeroes = ''
+
+    for (let i = 0; i < number_of_leading_zeroes; i++) {
+        required_zeroes += '0'
+    }
 
     let number = 0
     let first_5_digits = ''
-    while (first_5_digits !== '00000') {
+    while (first_5_digits !== required_zeroes) {
         let to_encrypt = params + number
         first_5_digits = CryptoJS.MD5(to_encrypt)
             .toString()
             .split('')
-            .slice(0, 5)
+            .slice(0, number_of_leading_zeroes)
             .join('')
         number++
     }
--- 04/tests.js
+++ 04/tests.js
@@ -1,10 +1,11 @@
-function test_md5(description, params, expected) {
+function test_md5(description, params, leading_zeroes, expected) {
 
     QUnit.test(description, function (assert) {
-        assert.equal(calculate_md5(params), expected, description + " Passed!");
+        assert.equal(calculate_md5(params, leading_zeroes), expected, description + " Passed!");
     })
 }
 
-test_md5("md5 test", 'abcdef', 609043)
-test_md5("second md5 test", 'pqrstuv', 1048970)
-test_md5("puzzle input test", puzzle_input, 346386)
+test_md5("md5 test", 'abcdef', 5, 609043)
+test_md5("second md5 test", 'pqrstuv', 5, 1048970)
+test_md5("puzzle input test", puzzle_input, 5, 346386)
+test_md5("puzzle input test", puzzle_input, 6, 9958218)
